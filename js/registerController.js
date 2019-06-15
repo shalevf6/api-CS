9ef0d40c5d1cd3d26d6a2f9e63ba5d71fe91191d
idsh <idsh@post.bgu.ac.il>
2019-06-15 20:53:34 +0300

    idan_third_update

--- js/registerController.js
+++ js/registerController.js
@@ -3,33 +3,57 @@ app.controller('registerController', function($scope, $http, $window){
 
     let request = {
         method: 'GET',
+        url : 'http://localhost:3000/categories',
+        headers: {
+            "Content-Type" : "application/json",
+            "Access-Control-Allow-Origin" : "*"
+        }
+    };
+
+    $http(request)
+        .then(function success(response){
+                let categories = response.data;
+                $scope.categories = categories;
+            },
+            function error(err){
+                console.log("error! info: " + err);
+                $scope.username = err
+            })
+
+
+    let request1 = {
+        method: 'GET',
         url : 'http://localhost:3000/questions',
         headers: {
             "Content-Type" : "application/json",
             "Access-Control-Allow-Origin" : "*"
         }
     };
-       $http(request)
+
+    $http(request1)
         .then(function success(response){
-                let ans = response.data;
-                $scope.questions = ans;
+                let questions = response.data;
+                $scope.questions = questions;
             },
             function error(err){
                 console.log("error! info: " + err);
                 $scope.username = err
             })
-    let request1 = {
+
+
+    let request2 = {
         method: 'GET',
-        url : 'http://localhost:3000/poiCategory/:category',
+        url : 'http://localhost:3000/categories',
         headers: {
             "Content-Type" : "application/json",
             "Access-Control-Allow-Origin" : "*"
         }
     };
-    $http(request1)
+
+    $http(request2)
         .then(function success(response){
-                let ans = response.data;
-                $scope.categories = ans;
+                let categories = response.data;
+                $scope.categories = categories;
             },
             function error(err){
                 console.log("error! info: " + err);
